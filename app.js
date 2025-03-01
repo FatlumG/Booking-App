a714c8ac32db3648adc42a0c3dfdc2aeae990192
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-03-01 10:59:46 +0100

    Connected with database

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+connectDB();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
