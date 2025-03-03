3ac009fc531f111176948b281f3526298dac2448
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-03-03 12:03:43 +0100

    Created book model

---
+++ modules/bookings/book.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    clientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    menuId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Menu",
+      required: true,
+    },
+    bookingStatus: {
+      type: String,
+      enum: ["pending", "confirmed", "canceled", "completed", "not-show"],
+      default: "pending",
+    },
+    bookingDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.Model("Book", bookSchema);
+
+export default Book;
